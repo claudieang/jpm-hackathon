d30e526f7f69dcc18c9c1e77d19c431c1eb839ce
Claudie <claudie.ang.2014@sis.smu.edu.sg>
2019-06-27 13:02:21 +0800

    reddit shit

--- Twitter-data-Terry/DataCleaning.py
+++ Twitter-data-Terry/DataCleaning.py
@@ -1,14 +1,20 @@
 import pandas as pd
 
-data = pd.read_csv("jpm-12Aug17-2015.csv",sep = ';,',encoding="utf-8", error_bad_lines=False)
-data.columns = ['user','date','retweets','likes','text','geo','mention','tag']
-tmp = data[~data['user'].str.contains('jpm')]
+data = pd.read_csv("jpm-12Aug17-2015.csv",sep = ',',encoding="utf-8", error_bad_lines=False)
+# data
+data["combine"] =data["geo"] + " " + data["mentions"] + " " + data["hashtag"]+ " " + data["Unnamed: 8"]+ " " + data["Unnamed: 9"]+ " " + data["Unnamed: 10"] + " " + data["Unnamed: 11"] + " " + data["Unnamed: 12"] + " " + data["Unnamed: 13"]+ " " + data["Unnamed: 14"]
+data.drop(['geo', 'mentions', 'hashtag','Unnamed: 8', 'Unnamed: 9', 'Unnamed: 10','Unnamed: 11','Unnamed: 12','Unnamed: 13','Unnamed: 14',], axis=1, inplace=True)
+data["text"] = data["text"] + " " + data["combine"]
+del data["combine"]
+tmp = data[~data['username'].str.contains('jpm')]
 tmp.to_csv("jpm-12Aug17-2015-clean.csv",encoding='utf-8-sig',index=False)
 
 
-data = pd.read_csv("jpm-today-12Aug17.csv",sep = ';,',encoding="utf-8", error_bad_lines=False)
-data.columns = ['user','date','retweets','likes','text','geo','mention','tag']
-tmp = data[~data['user'].str.contains('jpm')]
-tmp.to_csv("jpm-today-12Aug17-clean.csv",encoding='utf-8-sig',index=False)
-
-
+data = pd.read_csv("jpm-today-12Aug17.csv",sep = ',',encoding="utf-8", error_bad_lines=False)
+# data
+data["combine"] =data["geo"] + " " + data["mentions"] + " " + data["hashtag"]+ " " + data["Unnamed: 8"]+ " " + data["Unnamed: 9"]+ " " + data["Unnamed: 10"] + " " + data["Unnamed: 11"] + " " + data["Unnamed: 12"] + " " + data["Unnamed: 13"]+ " " + data["Unnamed: 14"]
+data.drop(['geo', 'mentions', 'hashtag','Unnamed: 8', 'Unnamed: 9', 'Unnamed: 10','Unnamed: 11','Unnamed: 12','Unnamed: 13','Unnamed: 14',], axis=1, inplace=True)
+data["text"] = data["text"] + " " + data["combine"]
+del data["combine"]
+tmp = data[~data['username'].str.contains('jpm')]
+tmp.to_csv("today-12Aug17-clean.csv",encoding='utf-8-sig',index=False)
